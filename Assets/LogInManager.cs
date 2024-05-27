dc395306c46f96fa8d9f3e687f6a8a93072a9d79
mikewatouski <julirozenfarb@gmail.com>
2024-05-27 09:08:38 -0300

    Terminado

--- Assets/LogInManager.cs
+++ Assets/LogInManager.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogInManager : MonoBehaviour
 {
+
+    string rightPassword = "julo";
+    public InputField inputField;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void checkLogIn(string password, string input)
+    {
+        if (input == password)
+        {
+            Debug.Log("Contraseña Correcta");
+        }
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+
+    public void buttonClick()
+    {
+        checkLogIn(rightPassword, inputField.text);
     }
 }
